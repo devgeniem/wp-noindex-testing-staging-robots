3c33c5d0270251c4a1b260b969781a92be3dc893
Onni Hakala <onni@keksi.io>
2016-10-30 20:48:48 +0200

    Add plugin.php and update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wp-noindex-testing-staging-robots
-Deny site from robots.txt if it's accessed in WP_ENV staging or testing or with testing domain
+
+Deny site from `/robots.txt` if it's accessed with `WP_ENV=staging` or `WP_ENV=testing`.
+
+Also deny if this site is accessed with domain that ends like `WP_HIDE_ROBOTS_DOMAINS`.
+
+## Example config
+```php
+// hides site from robots if its accessed from any geniem.io or example.com subdomain.
+define('WP_HIDE_ROBOTS_DOMAINS','geniem.io,example.com');
+```
---
+++ plugin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin name: Hide Development/Staging From Indexing
+ * Description: Uses robots.txt to not index this site with search-engines
+ * Plugin author: Onni Hakala / Geniem Oy
+ * Plugin URI: https://github.com/devgeniem/wp-noindex-testing-staging-robots
+ * License URI: https://opensource.org/licenses/MIT
+ * Version: 1.0
+ */
+
+/**
+ * Hides staging/development from Search engines
+ *
+ * @param string $output - Output into robots.txt.
+ * @param bool   $public - 1 or 0 depending if blog public meta is turned on.
+ */
+add_filter( 'robots_txt', function( string $output, bool $public ) {
+
+    // Hides all non production sites and all sites under WP_HIDE_ROBOTS_DOMAIN
+    if ( defined( 'WP_ENV' ) && WP_ENV !== 'production' ) {
+        $output = "User-agent: *\n";
+        $output .= "Disallow: /\n";
+    } elseif ( defined('WP_HIDE_ROBOTS_DOMAINS') ) {
+
+        // Check all domains
+        $domains = explode(',', WP_HIDE_ROBOTS_DOMAINS );
+        foreach ($domains as $domain) {
+
+            // If domain contains any of the domains in WP_HIDE_ROBOTS_DOMAINS deny it from robots
+            if ( strpos( $_SERVER['HTTP_HOST'], $domain ) !== false ) {
+                $output = "User-agent: *\n";
+                $output .= "Disallow: /\n";
+                break;
+            }
+        }
+    }
+
+    return $output;
+}, 10, 2 );
